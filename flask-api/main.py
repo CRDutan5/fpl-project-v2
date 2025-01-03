98014270b262a19cf727ca736af2c2374a32320e
Carlitos <carlitosdutan@pursuit.org>
2025-01-03 13:55:25 -0500

    added extra route to compare two players at the same time rather than making two different fetch calls on the frontend

--- flask-api/main.py
+++ flask-api/main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 from script import parse_to_api
 import json
+import requests
 
 # Load environment variables
 load_dotenv()
@@ -12,7 +13,6 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
-# Retrieve team key from environment variables
 my_team_key = os.getenv("MY_TEAM_ID")
 if not my_team_key:
     raise ValueError("MY_TEAM_ID is not set in the environment variables")
@@ -24,12 +24,27 @@ def get_team_alternatives():
     This calls the `parse_to_api` function dynamically to ensure data is up-to-date.
     """
     try:
-        information = parse_to_api()  # Get fresh data
-        # Ensure the JSON string is parsed into a Python dictionary before jsonify
+        information = parse_to_api()  
         data = json.loads(information)
         return jsonify(data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+@app.route(f"/api/player/<int:player_id>/altplayer/<int:altPlayer_id>", methods=["GET"])
+def get_player_data(player_id, altPlayer_id):
+    try:
+        url = f"https://fantasy.premierleague.com/api/element-summary/{player_id}/"
+        url2 = f"https://fantasy.premierleague.com/api/element-summary/{altPlayer_id}/"
+        response = requests.get(url)
+        response2 = requests.get(url2)
+        response.raise_for_status()
+        response2.raise_for_status()
+
+        final_response = {player_id: response.json(), altPlayer_id: response2.json()}
+        return jsonify(final_response)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     # Set debug=False for production
